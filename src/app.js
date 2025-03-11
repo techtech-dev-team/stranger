f962c59d6d08e2d066e72a1e499df79873cf4698
Piyush Katole <katolepiyush16@gmail.com>
2025-03-11 16:24:49 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); // Middleware to parse JSON data
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
 
