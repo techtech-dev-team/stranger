4fc7aed768ec8ee302e7c5bdda8f10ad5fc22c8d
khushiak44555 <khushikumar44555@gmail.com>
2025-03-12 15:55:34 +0530

    changes

--- src/models/Expense.js
+++ src/models/Expense.js
@@ -5,7 +5,10 @@ const expenseSchema = new mongoose.Schema({
   paidTo: { type: String, required: true },
   reason: { type: String, required: true },
   amount: { type: Number, required: true },
-  verified: { type: Boolean, default: false }
+  verified: { type: Boolean, default: false },
+  regionIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Region' }],
+  branchIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Branch' }],
+  centreIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Centre' }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('Expense', expenseSchema);
