e5e6254fa233067e9807f4cb91b77ac3b80c1778
Piyush Katole <katolepiyush16@gmail.com>
2025-03-13 14:57:03 +0530

    chnages doene

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers, getUserById, registerUser, updateUser, deleteUser , login , markAbsent , getAttendanceReport, getMonthlyAttendanceReport} = require("../controllers/userController");
+const { getAllUsers, getUserById,getPresentStaffByDate, registerUser, updateUser, deleteUser , login , markAbsent , getAttendanceReport, getMonthlyAttendanceReport} = require("../controllers/userController");
 
 // Routes
 router.get("/", getAllUsers); // Get all users (Protected)
