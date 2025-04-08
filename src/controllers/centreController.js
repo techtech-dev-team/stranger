ab471af73917d480ed6f282dd784e2693891d03e
Piyush Katole <katolepiyush16@gmail.com>
2025-04-08 11:30:56 +0530

    Add balance and previousBalance to centre statistics response

--- src/controllers/centreController.js
+++ src/controllers/centreController.js
@@ -293,6 +293,8 @@ exports.getCentreStatistics = async (req, res) => {
     const centreStats = centres.map(centre => ({
       centreId: centre._id,
       name: centre.name,
+      balance: centre.balance,
+      previousBalance: centre.previousBalance,
       shortCode: centre.shortCode,
       totalClients: 0,
       totalSales: 0
