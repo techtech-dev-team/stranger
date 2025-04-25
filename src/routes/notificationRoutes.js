7423396f0c99eeaa8f2f8643f92d7cd0a3bb2b75
PRAVIN ABASAHEB KHAIRE <khairepravin14@gmail.com>
2025-04-25 16:20:43 +0530

    Update notificationRoutes.js

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -15,10 +15,13 @@ router.post('/check-missed-entries', async (req, res) => {
 
 // GET - Live Notifications using SSE
 router.get("/notifications", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://test.st9.in"); // Allow your frontend origin
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-  res.flushHeaders(); // force immediate send of headers
+  res.flushHeaders(); // Force headers to be sent now
 
   const sendEvent = (data) => {
     res.write(`data: ${JSON.stringify(data)}\n\n`);
@@ -27,7 +30,7 @@ router.get("/notifications", (req, res) => {
   sendEvent({ message: "SSE connected" });
 
   const keepAlive = setInterval(() => {
-    res.write(`: ping\n\n`); // comment line to keep connection alive
+    res.write(`: ping\n\n`);
   }, 15000);
 
   req.on("close", () => {
