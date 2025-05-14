ddb604695a07589312691fb5c5718294b322f118
PRAVIN ABASAHEB KHAIRE <khairepravin14@gmail.com>
2025-05-14 19:50:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config(); // Load environment variables
 require('./src/cron/dayBalanceJob');
-require('./src/cron/dailySummaryCron');
+require('./src/cron/DailySummaryCron');
 const http = require("http");
 const app = require("./src/app"); // Import the app from src/app.js
 
